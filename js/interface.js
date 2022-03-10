6df0631751295e3318d4cc1d380243a998093b06
Tony Wu <tonytlwu@users.noreply.github.com>
2022-03-10 10:11:19 +0000

    Linting and cleanup

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,8 @@
 Fliplet.Widget.autosize();
-Fliplet.Studio.emit('widget-save-label-update', {text: ''});
+Fliplet.Studio.emit('widget-save-label-update', { text: '' });
 
 var data = Fliplet.Widget.getData();
+
 if (data.dsn) {
   var position = data.dsn.indexOf('@');
   var dsn = [
@@ -9,15 +10,18 @@ if (data.dsn) {
     ':' + data.private.secretKey,
     data.dsn.slice(position)
   ].join('');
+
   $('#dsn').val(dsn);
 }
 
 // Fired from Fliplet Studio when the external save button is clicked
 Fliplet.Widget.onSaveRequest(function saveSettings() {
   var dsn = ''; // $('#dsn').val(); Use this to enable clients set own tracker
+
   if (dsn !== '') {
     var regex = /https:\/\/([a-z0-9]+):([a-z0-9]+)@sentry.io\/[0-9]+/g;
     var match = regex.exec(dsn);
+
     if (!match || match.length !== 3) {
       return Fliplet.Navigate.popup({
         popupMessage: '- Confirm your DSN. DSN must include the secret key.',
@@ -35,7 +39,7 @@ Fliplet.Widget.onSaveRequest(function saveSettings() {
     data = {
       dsn: null,
       private: {}
-    }
+    };
   }
 
   Fliplet.Widget.save(data).then(Fliplet.Widget.complete);
